c84b2eba70499b8889d3ef7276b327f80cad308d
Teemu L <teemulaaks@gmail.com>
2023-11-22 16:45:04 +0200

    Initial commit

---
+++ src/worker.ts
@@ -0,0 +1,65 @@
+import PostalMime from 'postal-mime';
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
+	//
+	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
+	// MY_QUEUE: Queue;
+}
+
+interface EmailMessage<Body = unknown> {
+	readonly from: string;
+	readonly to: string;
+	readonly headers: Headers;
+	readonly raw: ReadableStream;
+	readonly rawSize: number;
+
+	setReject(reason: String): void;
+	forward(rcptTo: string, headers?: Headers): Promise<void>;
+	reply(message: EmailMessage): Promise<void>;
+}
+
+async function streamToArrayBuffer(stream: ReadableStream, streamSize: number) {
+	let result = new Uint8Array(streamSize);
+	let bytesRead = 0;
+	const reader = stream.getReader();
+	while (true) {
+		const { done, value } = await reader.read();
+		if (done) {
+			break;
+		}
+		result.set(value, bytesRead);
+		bytesRead += value.length;
+	}
+	return result;
+}
+
+export default {
+	async email(message: EmailMessage, env: any, ctx: any) {
+		const rawEmail = await streamToArrayBuffer(message.raw, message.rawSize);
+		const parser = new PostalMime();
+		const parsedEmail = await parser.parse(rawEmail);
+		await fetch('https://parse.laaksonen.eu/api/parse', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				from: message.from,
+				subject: message.headers.get('subject'),
+				text: parsedEmail.text,
+			}),
+		});
+		await message.forward('teemulaaks@gmail.com');
+	},
+};
